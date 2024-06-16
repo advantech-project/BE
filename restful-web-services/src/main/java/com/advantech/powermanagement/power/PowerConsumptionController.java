0625ee23abebbdd23e48ef3468d94bc6e90354c9
kijeong <rlwjddl1596@naver.com>
2024-06-17 04:00:29 +0900

    hi

--- restful-web-services/src/main/java/com/advantech/powermanagement/power/PowerConsumptionController.java
+++ restful-web-services/src/main/java/com/advantech/powermanagement/power/PowerConsumptionController.java
@@ -15,14 +15,14 @@ public class PowerConsumptionController {
         buildings[2] = "5호북관";
         buildings[3] = "5호남관";
         buildings[4] = "60주년기념관";
-        buildings[5] = "하이테크";
-        buildings[6] = "본관";
+        buildings[5] = "하이테크센터";
+        buildings[6] = "1호관(본관)";
         buildings[7] = "2호북관";
         buildings[8] = "2호남관/4호관";
         buildings[9] = "서호관";
         buildings[10] = "인하드림센터 2/3관";
-        buildings[11] = "9호관";
-        buildings[12] = "7호관";
+        buildings[11] = "9호관/평생교육관";
+        buildings[12] = "7호관(학생회관)";
         buildings[13] = "로스쿨관";
         buildings[14] = "김현태인하드림센터";
 
